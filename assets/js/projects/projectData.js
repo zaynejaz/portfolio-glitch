443808c32dc2230f9d2cee5438a0c452cae45e9b
zaynejaz <zayn.ejaz@gmail.com>
2022-02-13 11:02:20 +0300

    added html email data

--- assets/js/projects/projectData.js
+++ assets/js/projects/projectData.js
@@ -1,128 +1,330 @@
+// --------------====================HTML EMAIL DATA====================--------------
+const HE_DATA=[
+    {
+        projectTitle:   "Apple Watch",
+        projectURL:     "https://ze-email1-applewatch.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail3-Promotional",
+        btnText:        "view code"
+    },
+    {
+        projectTitle:   "Netflix | The Punisher: Frank",
+        projectURL:     "https://ze-email2-punisherfrank.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail4-Promotional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Netflix | The Punisher: Jigsaw",
+        projectURL:     "https://ze-email2-punisherjigsaw.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail4.2-Promotional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Netflix | The Punisher: Bullet Skull",
+        projectURL:     "https://ze-email2-punisherskull.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail4.3-Promotional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Netflix | The Punisher: Coming Soon",
+        projectURL:     "https://ze-email2-punishergif.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail4.4-Promotional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "TuneIn Premium",
+        projectURL:     "https://ze-email3-tunein.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail5-Promotional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "InVision Studio",
+        projectURL:     "https://ze-email4-invision.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail6-Newsletter",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "National Geographic: Issue 169",
+        projectURL:     "https://ze-email5-natgeo.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail7-Newsletter",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Pandora Premium",
+        projectURL:     "https://ze-email6-pandora.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail8-Transactional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "SquareSpace",
+        projectURL:     "https://ze-email7-squarespace.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail9-Transactional",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Apple: New Products",
+        projectURL:     "https://ze-email8-applenewproducts.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/HTMLEmail10-Multi",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Andrea Iyamah",
+        projectURL:     "https://zaynejaz.github.io/klaviyo/",
+        projectRepo:    "https://github.com/zaynejaz/klaviyo",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "MailerLite: Welcome",
+        projectURL:     "https://zaynejaz.github.io/MailerLite/welcomeEmail/",
+        projectRepo:    "https://zaynejaz.github.io/MailerLite/",
+        btnText:        "view exhibition"   
+    },
+    {
+        projectTitle:   "Altassian",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-altassianGit/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-altassianGit",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Design Modo",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-designModoBlackFriday/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-designModoBlackFriday",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Disney+ | Black Widow",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-disneyBlackWidow/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-disneyBlackWidow",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Disney+ | Subscription",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-disneySubscription/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-disneySubscription",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "F1 TV - Pro",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-f1TvPro/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-f1TvPro",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Ferrari Store",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-ferrariGifts/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-ferrariGifts",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Ferrari: Newsletter November",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-ferrariNewsNov/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-ferrariNewsNov",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Google Store",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-googleBlackFriday/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-googleBlackFriday",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Google Domains",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-googleDomains/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-googleDomains",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Louie Spencer: Masterclass<",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-louiespenceMasterclass/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-louiespenceMasterclass",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "MacPaw: CleanMyMac X",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-macpawDiscover/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-macpawDiscover",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "MetaLab",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-metalabQuaterly/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-metalabQuaterly",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "MeUndies: Reciept",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-meUndiesThankYou/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-meUndiesThankYou",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Microsoft",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-microsoftROI/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-microsoftROI",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "National Geographic: Halloween",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-natGeoHalloween/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-natGeoHalloween",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "National Geographic: Inspire",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-natGeoInspire/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-natGeoInspire",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Netflix | Coming Soon",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-netflixComingSoon/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-netflixComingSoon",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Netflix | Kids",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-netflixKids/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-netflixKids",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Playstation | Wrap Up",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-playstationWrapUp/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-playstationWrapUp",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Under Armour: Train Harder",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-underarmourTraining/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-underarmourTraining",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Under Armour: Welcome",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-underarmourWelcome/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-underarmourWelcome",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "YouTube + Google One",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-youtubeGoogleOne/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-youtubeGoogleOne",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "YouTube Music",
+        projectURL:     "https://zaynejaz.github.io/htmlemail-youtubeMusic/",
+        projectRepo:    "https://github.com/zaynejaz/htmlemail-youtubeMusic",
+        btnText:        "view code"   
+    },
+
+    // {
+    //     projectTitle:   "",
+    //     projectURL:     "",
+    //     projectRepo:    "",
+    //     btnText:        "view code"   
+    // },
+    
+
+] 
+
+HE_DATA.reverse();
+
+function dataPrinterHTML_E(){
+    let data_output = '';
+    for(let i = 0; i < HE_DATA.length; i++) {
+        data_output += '<li><a href="' + HE_DATA[i].projectURL + '" class="projectLink" target="_blank" rel="noopener noreferer">' + HE_DATA[i].projectTitle + '</a><a href="' + HE_DATA[i].projectRepo + '" class="codeLink repoURL" target="_blank" rel="noopener noreferer">' + HE_DATA[i].btnText + '</a></li>';  
+    }
+    return data_output;
+}
+
+document.getElementById("htmlEmail-data").innerHTML= dataPrinterHTML_E(); 
+
 
 // --------------====================LANDING PAGE DATA====================--------------
 
 const LP_DATA=[
     {
-        projectTitle:   "DJI - Inspire II", 
-        projectURL:     "https://ze-inspire.netlify.app/",  
+        projectTitle:   "DJI - Inspire II",
+        projectURL:     "https://ze-inspire.netlify.app/",
         projectRepo:    "https://github.com/zaynejaz/DJInspire",
         btnText:        "view code"
     },
     {
-        projectTitle:   "2", 
-        projectURL:     "2",  
-        projectRepo:    "2",
+        projectTitle:   "Coding Phase",
+        projectURL:     "https://ze-htmlemaildeveloper.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/CodingPhase",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "MJSTK",
+        projectURL:     "https://ze-mjstk.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/LandingPageMJSTK",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "FlexiFarm",
+        projectURL:     "https://ze-flexifarm.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/Flexifarm-LP",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "FlexiFarm | SignUp",
+        projectURL:     "https://ze-flexifarmsignup.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/Flexifarm-signup",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Microsoft",
+        projectURL:     "https://ze-microsoft.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/Clone-Microsoft",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "The Email Company",
+        projectURL:     "https://ze-theemailcompany.netlify.app/",
+        projectRepo:    "https://github.com/zaynejaz/nextjs_landing_page",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "GPT-3",
+        projectURL:     "https://ze-gpt3.netlify.app",
+        projectRepo:    "https://github.com/zaynejaz/react-lp-gpt3",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "The Alpha Agency",
+        projectURL:     "https://the-alpha-agency.vercel.app/",
+        projectRepo:    "https://github.com/zaynejaz/react-agency-a",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "The Beta Agency",
+        projectURL:     "https://the-beta-agency.vercel.app/",
+        projectRepo:    "https://github.com/zaynejaz/react-agency-b",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "The Gamma Agency",
+        projectURL:     "https://the-gamma-agency.vercel.app/",
+        projectRepo:    "https://github.com/zaynejaz/react-agency-c",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "Le Creative",
+        projectURL:     "https://le-creative.vercel.app",
+        projectRepo:    "https://github.com/zaynejaz/react-lp-creative",
+        btnText:        "view code"   
+    },
+    {
+        projectTitle:   "La Digital",
+        projectURL:     "https://la-digital.vercel.app/",
+        projectRepo:    "https://github.com/zaynejaz/react-lp-digital",
         btnText:        "view code"   
     },
 
     // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
-    //     projectRepo:    "",
-    //     btnText:        "view code"   
-    // },
-
-    // {
-    //     projectTitle:   "", 
-    //     projectURL:     "",  
+    //     projectTitle:   "",
+    //     projectURL:     "",
     //     projectRepo:    "",
     //     btnText:        "view code"   
     // },
